75a6892c2dd55c2e21d3ad07bdf0c45b4b57ad25
Cody Allen <ceedubs@gmail.com>
2019-01-10 06:48:37 -0800

    Add tests for runNFAShortInput

---
+++ regex/src/test/scala/NFATests.scala
@@ -0,0 +1,16 @@
+package ceedubs.irrec
+package regex
+
+import RegexAndCandidate._
+
+class NFATests extends IrrecSuite {
+  test("runNFA and runNFAShortInput consistency"){
+    forAll{ rc: RegexAndCandidate[Int] =>
+      val nfa = Glushkov.kleeneToNFA(rc.r)
+      val matches: (Match[Int], Int) => Boolean = _.matches(_)
+      val short = NFA.runNFAShortInput[Stream, Int, Match[Int], Int](nfa, matches)
+      val normal = NFA.runNFA[Stream, Int, Match[Int], Int](nfa, matches)
+      short(rc.candidate) should ===(normal(rc.candidate))
+    }
+  }
+}
