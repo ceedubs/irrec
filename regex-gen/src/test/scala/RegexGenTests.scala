95949d9cc57858194964b7aabc8600cd71bbb474
Cody Allen <ceedubs@gmail.com>
2019-01-11 08:36:50 -0800

    Add test for regexMatchingStringGen

--- regex-gen/src/test/scala/RegexGenTests.scala
+++ regex-gen/src/test/scala/RegexGenTests.scala
@@ -1,13 +1,26 @@
 package ceedubs.irrec
 package regex
 
+import RegexGen._
+
 import cats.tests.CatsSuite
 import org.scalacheck.Arbitrary.arbitrary
 
 class RegexGenTests extends CatsSuite {
-  test("regexMatchingStringGen generates a failing stream for Zero"){
+  test("regexMatchingStreamGen generates a failing stream for Zero"){
     val r: Regex[Char] = Regex.impossible
     val gen = RegexGen.regexMatchingStreamGen(arbitrary[Char]).apply(r)
     gen.sample should ===(None)
+  }
+
+  test("regexMatchingStringGen is consistent with RegexMatchingStreamGen"){
+    val gen = for {
+      r <- arbitrary[Regex[Char]]
+      s <- RegexGen.regexMatchingStringGen(arbitrary[Char]).apply(r)
+    } yield (r, s)
+
+    forAll(gen){ case (r, s) =>
+      r.stringMatcher(s) should ===(true)
+    }
   }
 }
