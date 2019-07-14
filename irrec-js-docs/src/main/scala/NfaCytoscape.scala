d9169d01b3b115cbbf3a1545ed4241d23ac53b97
Cody Allen <ceedubs@gmail.com>
2019-07-13 20:57:44 -0700

    formatting

--- irrec-js-docs/src/main/scala/NfaCytoscape.scala
+++ irrec-js-docs/src/main/scala/NfaCytoscape.scala
@@ -10,26 +10,31 @@ import org.scalajs.dom.raw.Element
 
 object NfaCytoscape {
   def nfaGraphDict(nfa: NFA[Int, Match[Char]], container: Element): js.Dictionary[js.Any] = {
-    val (nodes, transitions) = nfa.transitions.toList.foldMap{ case (node1, transitions) =>
-      transitions.foldMap{ case (node2, m) =>
-        (Set(node2), List((node1, node2, showCharMatch(m))))
-      } |+| ((Set(node1), List.empty))
+    val (nodes, transitions) = nfa.transitions.toList.foldMap {
+      case (node1, transitions) =>
+        transitions.foldMap {
+          case (node2, m) =>
+            (Set(node2), List((node1, node2, showCharMatch(m))))
+        } |+| ((Set(node1), List.empty))
     } |+| ((nfa.initStates ++ nfa.finalStates, List.empty))
 
-    val nodeDicts = nodes.toList.map{ node =>
-      js.Dictionary[js.Any]("data" -> js.Dictionary[js.Any](
-        "id" -> node,
-        "initState" -> nfa.initStates.contains(node),
-        "finalState" -> nfa.finalStates.contains(node)))
+    val nodeDicts = nodes.toList.map { node =>
+      js.Dictionary[js.Any](
+        "data" -> js.Dictionary[js.Any](
+          "id" -> node,
+          "initState" -> nfa.initStates.contains(node),
+          "finalState" -> nfa.finalStates.contains(node)))
     }
 
-    val edgeDicts = transitions.map{ case (node1, node2, m) =>
-      js.Dictionary("data" ->
-        js.Dictionary[js.Any](
-          "id" -> s"$node1->$node2",
-          "source" -> node1,
-          "target" -> node2,
-          "label" -> m))
+    val edgeDicts = transitions.map {
+      case (node1, node2, m) =>
+        js.Dictionary(
+          "data" ->
+            js.Dictionary[js.Any](
+              "id" -> s"$node1->$node2",
+              "source" -> node1,
+              "target" -> node2,
+              "label" -> m))
     }
 
     js.Dictionary(
@@ -53,27 +58,23 @@ object NfaCytoscape {
             "text-background-shape" -> "roundrectangle",
             "text-background-padding" -> "3px",
             "text-background-color" -> "#17B890",
-            "text-background-opacity" -> 1)),
+            "text-background-opacity" -> 1
+          )
+        ),
         js.Dictionary(
           "selector" -> "node[!initState, !finalState]",
-          "style" -> js.Dictionary[js.Any](
-            "shape" -> "ellipse",
-            "height" -> "1px",
-            "width" -> "1px")),
+          "style" -> js
+            .Dictionary[js.Any]("shape" -> "ellipse", "height" -> "1px", "width" -> "1px")),
         js.Dictionary(
           "selector" -> "node[?finalState]",
-          "style" -> js.Dictionary[js.Any](
-            "shape" -> "star",
-            "height" -> "14px",
-            "width" -> "14px")),
+          "style" -> js
+            .Dictionary[js.Any]("shape" -> "star", "height" -> "14px", "width" -> "14px")),
         js.Dictionary(
           "selector" -> "node[?initState]",
-          "style" -> js.Dictionary[js.Any](
-            "shape" -> "vee",
-            "width" -> "14px",
-            "height" -> "14px"))),
-      "elements" -> js.Dictionary(
-        "nodes" -> js.Array(nodeDicts: _*),
-        "edges" -> js.Array(edgeDicts: _*)))
+          "style" -> js.Dictionary[js.Any]("shape" -> "vee", "width" -> "14px", "height" -> "14px"))
+      ),
+      "elements" -> js
+        .Dictionary("nodes" -> js.Array(nodeDicts: _*), "edges" -> js.Array(edgeDicts: _*))
+    )
   }
 }
