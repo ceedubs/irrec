68ed2bdbdf0a2db54b71de3bf759c140881a3955
Cody Allen <ceedubs@gmail.com>
2019-07-18 20:16:58 -0700

    WIP capturing

--- regex/src/main/scala/LabeledKleene.scala
+++ regex/src/main/scala/LabeledKleene.scala
@@ -1,6 +1,6 @@
 package ceedubs.irrec
 package regex
 
-final case class LabeledKleene[L, A](label: Option[L], regex: Kleene[A]) {
+final case class LabeledKleene[L, A](label: L, value: Kleene[A]) {
   def toCapturingKleene: CapturingKleene[L, A] = CapturingKleene.labeledKleene(this)
 }
