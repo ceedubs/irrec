9bcfc4d615867e568eb42f8208a90b8edb531d65
Cody Allen <ceedubs@gmail.com>
2019-07-18 20:16:58 -0700

    WIP capturing

--- regex/src/main/scala/Glushkov.scala
+++ regex/src/main/scala/Glushkov.scala
@@ -2,12 +2,15 @@ package ceedubs.irrec
 package regex
 
 import cats.Order
+import cats.data.State
 import cats.implicits._
 import higherkindness.droste.Algebra
 import higherkindness.droste.data.prelude._
 import higherkindness.droste.data.CoattrF
 import higherkindness.droste.scheme
 import scala.collection.immutable.SortedMap
+import ceedubs.irrec.regex.SemirngF.Plus
+import ceedubs.irrec.regex.SemirngF.Times
 
 /**
  * Functions for converting a regular expression into an NFA.
@@ -82,9 +85,80 @@ object Glushkov {
       }
     }
 
+  // TODO ceedubs document...this has a fairly specific use-case
+  def indexedLabel[A](lk: LabeledKleene[Boolean, A]): State[Int, LabeledKleene[Option[Int], A]] =
+    if (lk.label) State(i => (i + 1, lk.copy(label = Some(i))))
+    else State.pure(lk.copy(label = None))
+
+  def indexedLabels[A](
+    x: SemirngF[LabeledKleene[Boolean, A]]): State[Int, SemirngF[LabeledKleene[Option[Int], A]]] =
+    x match {
+      case Plus(l, r) => (indexedLabel(l), indexedLabel(r)).mapN(Plus.apply)
+      case Times(l, r) => (indexedLabel(l), indexedLabel(r)).mapN(Times.apply)
+    }
+
+  def indexLabeledKleeneSemirng[L, A](
+    x: SemirngF[LabeledKleene[L, A]]): State[Int, SemirngF[LabeledKleene[L, (Int, A)]]] =
+    x match {
+      case SemirngF.Plus(l, r) =>
+        (indexKleeneLeaves(l.value), indexKleeneLeaves(r.value)).mapN((k1, k2) =>
+          SemirngF.Plus(l.copy(value = k1), r.copy(value = k2)))
+      case SemirngF.Times(l, r) =>
+        (indexKleeneLeaves(l.value), indexKleeneLeaves(r.value)).mapN((k1, k2) =>
+          SemirngF.Times(l.copy(value = k1), r.copy(value = k2)))
+    }
+
+  def indexKleeneLeaves[A](k: Kleene[A]): State[Int, Kleene[(Int, A)]] =
+    scheme.cataM(CoattrSchemes.indexLeaves[KleeneF, A]).apply(k)
+
+  // TODO ceedubs should this be here?
+  final case class CapturingRegexLabelState(leafIndex: Int, captureIndex: Int)
+
+  object CapturingRegexLabelState {
+    def zoomState[S1, S2, A](state: State[S1, A])(set: S1 => S2, get: S2 => S1): State[S2, A] =
+      state.dimap(get)(set)
+
+    def zoomLeafIndex[A](state: State[Int, A]): State[CapturingRegexLabelState, A] = {
+      // TODO ceedubs
+      import cats.data.IndexedStateT
+      IndexedStateT.apply{ s =>
+        
+      }
+      state.dimap(_.leafIndex)(i => labels.copy(leafIndex = i))
+    }
+  }
+
+  // TODO ceedubs naming
+  def bolth[A](x: SemirngF[LabeledKleene[Boolean, A]]): State[CapturingRegexLabelState, SemirngF[LabeledKleene[Option[Int], (Int, A)]]] =
+    for {
+      labels <- State.get[CapturingRegexLabelState]
+      // this would be much cleaner with lenses...
+      withLabels <- indexedLabels(x).dimap((_: CapturingRegexLabelState).captureIndex)(i => labels.copy(captureIndex = i))
+      withIndices <- indexLabeledKleeneSemirng(withLabels).dimap((_: CapturingRegexLabelState).leafIndex)(i => labels.copy(leafIndex = i))
+    } yield withIndices
+
+  // TODO ceedubs
+  import higherkindness.droste.AlgebraM
+  import higherkindness.droste.data.Coattr
+  def bolthAlgebra[A]: AlgebraM[State[CapturingRegexLabelState, ?], CoattrF[SemirngF, LabeledKleene[Boolean, A], ?], Coattr[SemirngF, LabeledKleene[Option[Int], (Int, A)]]] =
+    AlgebraM[State[CapturingRegexLabelState, ?], CoattrF[SemirngF, LabeledKleene[Boolean, A], ?], Coattr[SemirngF, LabeledKleene[Option[Int], (Int, A)]]] {
+      CoattrF.un(_) match {
+        // TODO ceedubs this is repeated in a couple of places
+        case Left(l) => indexedLabel(l).dimap((_: CapturingRegexLabelState).leafIndex)(i => labels.copy(leafIndex = i)).map(Coattr.pure(_))
+        case Right(_) => 3
+      }
+    }
+
+  // TODO ceedubs naming
+  //def todo[A](k: CapturingKleene[Boolean, A]): NFA[Int, (Int, A)] = {
+  //  val x = AlgebraM(bolth(_))
+  //  //val indexed = scheme.cataM()
+  //  x
+  //}
+
   // TODO ceedubs can we combine indexing leaves with another algebra and do a single pass?
   def kleeneToNFA[A](k: Kleene[A]): NFA[Int, A] = {
-    val indexed = scheme.cataM(CoattrSchemes.indexLeaves[KleeneF, A]).apply(k).runA(1).value
+    val indexed = indexKleeneLeaves(k).runA(1).value
     val ll = scheme.cata(indexedKleeneToLocalLanguage[Int, A]).apply(indexed)
     LocalLanguage.intLocalLanguageToNFA(ll)
   }
--- regex/src/main/scala/SemiRngF.scala
+++ regex/src/main/scala/SemiRngF.scala
@@ -1,9 +1,20 @@
 package ceedubs.irrec
 package regex
 
-sealed abstract class SemirngF[+A] extends Product with Serializable
+import cats.~>
+
+sealed abstract class SemirngF[+A] extends Product with Serializable {
+  import ceedubs.irrec.regex.SemirngF._
+
+  def toKleeneF: KleeneF[A] = this match {
+    case Times(l, r) => KleeneF.Times(l, r)
+    case Plus(l, r) => KleeneF.Plus(l, r)
+  }
+}
 
 object SemirngF {
   final case class Times[+A](l: A, r: A) extends SemirngF[A]
   final case class Plus[+A](l: A, r: A) extends SemirngF[A]
+
+  val semirngFToKleeneF: SemirngF ~> KleeneF = λ[SemirngF ~> KleeneF](_.toKleeneF)
 }
