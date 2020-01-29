9bab7e11b97703e547cf221488cc0eff4119b052
Cody Allen <ceedubs@gmail.com>
2020-01-28 16:55:59 -0800

    Use foldLeftM in parseOnly to short-circuit input traversal

--- regex/src/main/scala/ParseState.scala
+++ regex/src/main/scala/ParseState.scala
@@ -29,9 +29,14 @@ final case class ParseState[In, A](queue: StateQueue[Thread[In, A]]) extends Any
   def results: List[A] = threads.flatMap(_.result)
 
   // TODO document
-  // TODO use foldLeftM to short-circuit?
   def parseOnly[F[_]](input: F[In])(implicit foldableF: Foldable[F]): Option[A] =
-    input.foldLeft(this)(_.step(_)).results.headOption
+    input
+      .foldLeftM(this) { (state, input) =>
+        if (state.queue.reversedElements.isEmpty)
+          Left(())
+        else Right(state.step(input))
+      }
+      .fold(_ => None, _.results.headOption)
 
   // TODO could add with ops class instead, but run into can't have value class wrapping value class
   def parseOnlyS(input: String)(implicit ev: Char Is In): Option[A] =
