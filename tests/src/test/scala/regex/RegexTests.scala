79e4bd3373b0373d86b0fab1aac92a4aa1d03ec6
Cody Allen <ceedubs@gmail.com>
2020-02-02 07:31:16 -0800

    Attempt to make AlternativeLaws check faster
    It looks like the test configuration wasn't getting picked up by the
    version of `check` that I was previously using.

--- tests/src/test/scala/regex/RegexTests.scala
+++ tests/src/test/scala/regex/RegexTests.scala
@@ -7,11 +7,11 @@ import gen.RegexGen._
 import cats.Eq
 import cats.implicits._
 import cats.laws.discipline.AlternativeTests
-import org.scalatestplus.scalacheck.Checkers.check
+import org.scalatestplus.scalacheck.Checkers
 
-class RegexTests extends IrrecSuite {
+class RegexTests extends IrrecSuite with Checkers {
   test("Regex Alternative laws") {
-    val gen = RegexEq.genRegexEq[Char, Match[Char]]
+    val gen = RegexEq.genRegexEq[Char, Match[Char]](5)
     forAll(gen, minSuccessful(1)) { rEqv =>
       implicit def mkRegexEq[Out](implicit eqOut: Eq[Out]): Eq[RegexC[Out]] = rEqv(eqOut)
       val ruleSet = AlternativeTests[RegexC].alternative[Int, Int, Int]
