9925f8b3b4a486358032313083ea36bdc21e683b
Cody Allen <ceedubs@gmail.com>
2020-10-22 08:56:08 -0700

    Explicitly start gpg agent
    Hopefully this will fix releasing

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,4 +3,8 @@
 
 set -euxo pipefail
 
+# Since we are using the quick Nix installation, the gpg agent isn't started by a daemon.
+# This may be a bit hacky, but we'll just explicitly load it.
+gpgconf --reload gpg-agent
+
 sbt ';ci-release;docs/docusaurusPublishGhpages'
