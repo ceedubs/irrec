220c2134b789ca6103aa41f31542609c9fce90cc
Cody Allen <ceedubs@gmail.com>
2020-10-22 08:33:51 -0700

    Add pinentry
    Hopefully this will fix publishing signed releases

--- nix/release-env/default.nix
+++ nix/release-env/default.nix
@@ -3,10 +3,14 @@ let
   dev-env = import ../dev-env { inherit pkgs; };
 in
   with pkgs;
-  stdenv.mkDerivation {
-    name = "irrec-release-env";
+  let
+    pinentry = if (stdenv.isDarwin) then pinentry_mac else pkgs.pinentry;
+  in
+    stdenv.mkDerivation {
+      name = "irrec-release-env";
 
-    buildInputs = [
-      gnupg
-    ] ++ dev-env.buildInputs;
-  }
+      buildInputs = [
+        gnupg
+        pinentry
+      ] ++ dev-env.buildInputs;
+    }
