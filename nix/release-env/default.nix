ea82190714b0df4020c023c74190260e4f17db55
Cody Allen <ceedubs@gmail.com>
2020-10-22 09:25:33 -0700

    Give up on Nix for GPG

--- nix/release-env/default.nix
+++ nix/release-env/default.nix
@@ -3,14 +3,11 @@ let
   dev-env = import ../dev-env { inherit pkgs; };
 in
   with pkgs;
-  let
-    pinentry = if (stdenv.isDarwin) then pinentry_mac else pkgs.pinentry;
-  in
-    stdenv.mkDerivation {
-      name = "irrec-release-env";
+  stdenv.mkDerivation {
+    name = "irrec-release-env";
 
-      buildInputs = [
-        gnupg
-        pinentry
-      ] ++ dev-env.buildInputs;
-    }
+    buildInputs = [
+    ] ++ dev-env.buildInputs;
+  }
+      
+
--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,4 @@
 
 set -euxo pipefail
 
-# Since we are using the quick Nix installation, the gpg agent isn't started by a daemon.
-# This may be a bit hacky, but we'll just explicitly load it.
-export GPG_TTY=$(tty)
-gpgconf --reload gpg-agent
-
 sbt ';ci-release;docs/docusaurusPublishGhpages'
